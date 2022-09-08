60b788eaf1f69a804709ed98b72f6f0f9abf80e3
Zakky <88846365+sakku116@users.noreply.github.com>
2022-09-08 09:50:50 +0700

    update

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"belajar-gin/explore/views"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-
+	fmt.Println("test")
 	router.GET("/", views.Index)
 	router.Run("localhost:7000")
 }
--- views/views.go
+++ views/views.go
@@ -1,13 +1,12 @@
 package views
 
 import (
-	// "fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
-	// data := map[string]interface{}{
-	// 	"request": "test",
-	// }
-	c.IndentedJSON(200, gin.H{"message": "hello world", "data": c.Request.Header})
+	data := map[string]interface{}{
+		"request": c.Header,
+	}
+	c.IndentedJSON(200, gin.H{"message": "hello world", "data": data})
 }
